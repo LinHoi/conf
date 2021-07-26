fd6320987791b2a73f6c4f56122c0e06ee9b4855
LinHui <linhui@baijia.com>
2021-07-26 14:39:19 +0800

    feat:init

---
+++ conf.go
@@ -0,0 +1,64 @@
+package conf
+
+import (
+	"github.com/pkg/errors"
+	"reflect"
+)
+
+type Option func(*options)
+
+type options struct {
+	file   string
+	apollo *Apollo // apollo配置信息
+}
+
+func WithFile(file string) Option {
+	return func(o *options) {
+		o.file = file
+	}
+}
+
+func checkFile() Option {
+	return func(o *options) {
+		if o.file == "" {
+			panic("file name is required")
+		}
+	}
+}
+
+func WithApollo(apollo *Apollo) Option {
+	return func(o *options) {
+		o.apollo = apollo
+	}
+}
+
+// New create a new config
+func New(config interface{}, opts ...Option) error {
+	if reflect.ValueOf(config).Type().Kind() != reflect.Ptr {
+		return errors.New("pointer is required")
+	}
+	o := newOption(opts...)
+	manager := &Manager{
+		file:   o.file,
+		config: config,
+		apollo: o.apollo,
+	}
+
+	err := manager.getConfFromFile()
+	if err != nil {
+		return nil
+	}
+
+	err = manager.getConfFromApollo()
+	return err
+}
+
+func newOption(opts ...Option) *options {
+	o := &options{}
+	opts = append(opts, checkFile())
+	for _, opt := range opts {
+		opt(o)
+	}
+
+	return o
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module conf
+
+go 1.16
+
+require (
+	github.com/fsnotify/fsnotify v1.4.9
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826
+	github.com/pkg/errors v0.9.1
+	github.com/shima-park/agollo v1.2.11
+	github.com/spf13/viper v1.8.1
+	go.uber.org/zap v1.18.1
+)
